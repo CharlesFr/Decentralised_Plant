2f24a74e42116dbb4299836c2aa266ad5321bc95
tom <powerth@tcd.ie>
2017-11-17 14:12:20 +0100

    [ADD] Changes to arduino code

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 var csv = require('./dataParser.js');
 
-var data = [];
+var data = [
+  ['date','close']
+];
 
-
+var count = 0;
 var five = require("johnny-five");
-var board = new five.Board();
+var board = new five.Board({repl: false});
 
 board.on("ready", function() {
 
   // Create a new generic sensor instance for
   // a sensor connected to an analog (ADC) pin
   var sensor = new five.Sensor("A0");
+  this.samplingInterval(20000);
 
   // When the sensor value changes, log the value
   sensor.on("change", function(value) {
-    var newData = [stringifyDate(), value.toString()];
-    console.log(newData);
-    data.push(newData);
+    var moisture = (1 - value/1024) * 100;
+    console.log("Moisture = " + moisture + "%");
+    data.push([stringifyDate(), moisture.toString()])
+    csv.writeDataToCSV(data, "./src/datatest.csv");
+
 
   });
 });
 
 
 function stringifyDate() {
-
+  var monthNames = ['Jan', 'Feb', 'Mar', 'Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var today = new Date();
+  var hh = today.getHours();
+  var ms = today.getMinutes();
+  var ss = today.getSeconds();
   var dd = today.getDate();
   var mm = today.getMonth() + 1; //January is 0!
-  var yyyy = today.getFullYear();
+  var yyyy = today.getFullYear() - 2000;
+  count+=1;
+
 
   if (dd < 10) {
     dd = '0' + dd
@@ -37,10 +47,6 @@ function stringifyDate() {
     mm = '0' + mm
   }
 
-  today = mm + '/' + dd + '/' + yyyy;
+  today = count%30 + '-' + monthNames[mm] + '-' + yyyy;
   return today;
 }
-
-
-
-csv.writeDataToCSV(data, "datatestc.csv");
